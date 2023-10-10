f8a02b22ca6ec152c6e4f3ffc1abcc375845b03b
Kestrel <jettb6@gmail.com>
2023-10-09 23:40:47 -0700

    `css` + `js` animation

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use actix_files as fs;
 use actix_web::{get, App, HttpServer, HttpRequest, Responder};
 
 #[get("/")]
-async fn index(req: HttpRequest) -> impl Responder {
+async fn index(_req: HttpRequest) -> impl Responder {
     fs::NamedFile::open("html/index.html")
 }
 
@@ -13,6 +13,8 @@ async fn main() -> std::io::Result<()> {
             .service(index)
             .service(fs::Files::new("/static", "./static").show_files_listing())
             .service(fs::Files::new("/js", "./js").show_files_listing())
+            .service(fs::Files::new("/css", "./css").show_files_listing())
+            .service(fs::Files::new("/fonts", "./fonts").show_files_listing())
     })
     .bind(("127.0.0.1", 8080))?
     .run()
--- ts/index.ts
+++ ts/index.ts
@@ -1,1 +1,45 @@
-console.log("hello world!")
+const CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789)@=$#^*/\\]}~_:;`";
+const FIRST_NAME = "Common"
+const LAST_NAME = "Kestrel";
+
+const randomChar = () => CHARS[Math.floor(Math.random() * (CHARS.length - 1))];
+const randomString = (length: number) => Array.from(Array(length)).map(randomChar).join("");
+
+const asciiToHex = (str: string) => {
+    let ret = [];
+    for (var n = 0, l = str.length; n < l; n ++) 
+     {
+		var hex = str.charCodeAt(n).toString(16).toUpperCase();
+		ret.push(hex);
+	 }
+     return ret.join(' ');
+}
+
+const firstHex = asciiToHex(FIRST_NAME);
+const lastHex = asciiToHex(LAST_NAME);
+
+const randomize = () => {
+    const wrapper = document.getElementById("string-wrapper")!;
+    const wrapperStyle = window.getComputedStyle(wrapper);
+
+    const fontHeight = parseFloat(wrapperStyle.fontSize);
+    const fontWidth = fontHeight / 2;
+
+    const box = wrapper.getBoundingClientRect();
+    const stringHeight = box.height;
+    const stringWidth = box.width;
+
+    const horizontalChars = Math.ceil(stringWidth / fontWidth);
+    const verticalChars = Math.ceil(stringHeight / fontHeight)
+    const stringLength = Math.ceil(verticalChars * horizontalChars);
+    
+    const firstIndex = stringLength / 2 - horizontalChars / 2 - 5;
+    const lastIndex = firstIndex + horizontalChars + 2; 
+    
+    const chars = randomString(stringLength);
+    const hex = asciiToHex(chars);
+    document.getElementById("string")!.innerHTML = chars.slice(0, firstIndex) + "<span class=\"redlight\">" + FIRST_NAME + "</span>" + chars.slice(firstIndex + FIRST_NAME.length, lastIndex) + "<span class=\"bluelight\">" + LAST_NAME + "</span>" + chars.slice(lastIndex + LAST_NAME.length);
+    document.getElementById("hex")!.innerHTML = hex.slice(0, 3*firstIndex) + "<span class=\"redlight\">" + firstHex + " </span>" + hex.slice(3*firstIndex + firstHex.length, 3*lastIndex) + "<span class=\"bluelight\">" + lastHex + " </span>" + hex.slice(3*lastIndex + lastHex.length);
+}
+
+setInterval(randomize, 50);
