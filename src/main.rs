6cedb535dc60ff64c37141a28a2eb0a055999a2a
Kestrel <jettb6@gmail.com>
2023-10-10 22:49:16 -0700

    Best practices

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,23 @@ async fn index(_req: HttpRequest) -> impl Responder {
     fs::NamedFile::open("html/index.html")
 }
 
+#[get("/favicon.ico")]
+async fn favicon(_req: HttpRequest) -> impl Responder {
+    fs::NamedFile::open("static/favicon.ico")
+}
+
+#[get("/robots.txt")]
+async fn robots(_req: HttpRequest) -> impl Responder {
+    fs::NamedFile::open("static/robots.txt")
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(index)
+            .service(favicon)
+            .service(robots)
             .service(fs::Files::new("/static", "./static").show_files_listing())
             .service(fs::Files::new("/js", "./js").show_files_listing())
             .service(fs::Files::new("/css", "./css").show_files_listing())
