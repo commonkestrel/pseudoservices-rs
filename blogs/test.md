0d63496bc2fb541059fb2014fb64a16b6e0f79be
Kestrel <jettb6@gmail.com>
2023-10-13 23:02:26 -0700

    Add blog templating with Markdown

---
+++ blogs/test.md
@@ -0,0 +1,14 @@
+# Test
+
+Just a test to see how this works.
+
+What about bullets?
+- single
+- double
+- triple?
+
+### Maybe tables?
+
+| h | e | a | d |
+|---|---|---|---|
+| wow | so cool | amazing | wonderful |
--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,96 @@
-use actix_files as fs;
-use actix_web::{get, App, HttpServer, HttpRequest, Responder};
-use serde::{Serialize, Deserialize};
-use lazy_static::lazy_static;
+use std::fs;
+use std::path::PathBuf;
+use actix_files as afs;
+use actix_web::{get, App, HttpRequest, HttpServer, Responder, HttpResponse, http::header::ContentType};
+use once_cell::sync::Lazy;
+use serde::{Deserialize, Serialize};
+use tera::{ Tera, Context };
+use pulldown_cmark::{ Parser, Options, html::push_html };
 
-lazy_static! {
-    static ref BLOGS: Vec<BlogPost> = serde_json::from_str(include_str!("blogs.json")).expect("unable to deserialize `blogs.json`");
-}
+static TEMPLATES: Lazy<Tera> = Lazy::new(|| Tera::new("templates/*.tera").expect("Failed to parse templates"));
+
+static BLOGS: Lazy<Vec<BlogPost>> = Lazy::new(|| {
+    serde_json::from_str(include_str!("../blogs/blogs.json")).expect("Unable to parse `blogs/blogs.json`")
+});
+
+static INDEX_CTX: Lazy<Context> = Lazy::new(|| {
+    Context::from_serialize(IndexCtx::from(BLOGS.clone())).expect("Unable to create `tera::Context` from `BlogPost`")
+});
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // Render and sanitize blog markdown
+    for blog in BLOGS.iter() {
+        let mut ctx = Context::new();
+        ctx.insert("title", &blog.title);
+        ctx.insert("thumbnail", &blog.thumbnail);
+
+        let md_path = PathBuf::from("./blogs").join(&blog.file);
+        let md = fs::read_to_string(md_path)?;
+
+        let options = Options::ENABLE_TABLES;
+        let md_parse = Parser::new_ext(&md, options);
+
+        let mut unsafe_html = String::new();
+        push_html(&mut unsafe_html, md_parse);
+
+        let safe_html = ammonia::clean(&unsafe_html);
+        ctx.insert("body", &safe_html);
+
+        println!("Rendered `{}`, output at `{}`", blog.file.display(), blog.out.display());
+        fs::write(&blog.out, TEMPLATES.render("blog.tera", &ctx).expect("Unable to render blog"))?;
+    }
+
     HttpServer::new(|| {
         App::new()
             .service(index)
             .service(favicon)
             .service(robots)
-            .service(fs::Files::new("/static", "./static").show_files_listing())
-            .service(fs::Files::new("/js", "./js").show_files_listing())
-            .service(fs::Files::new("/css", "./css").show_files_listing())
-            .service(fs::Files::new("/fonts", "./fonts").show_files_listing())
+            .service(afs::Files::new("/static", "./static").show_files_listing())
+            .service(afs::Files::new("/js", "./js").show_files_listing())
+            .service(afs::Files::new("/css", "./css").show_files_listing())
+            .service(afs::Files::new("/blog", "./blogs/out"))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize, Serialize, Clone)]
 struct BlogPost {
     title: String,
     description: String,
+    file: PathBuf,
+    out: PathBuf,
     href: String,
     thumbnail: String,
 }
 
+#[derive(Debug, Deserialize, Serialize)]
+struct IndexCtx {
+    blogs: Vec<BlogPost>,
+}
+
+impl From<Vec<BlogPost>> for IndexCtx {
+    fn from(value: Vec<BlogPost>) -> Self {
+        IndexCtx {blogs: value}
+    }
+}
+
 #[get("/")]
 async fn index(_req: HttpRequest) -> impl Responder {
-    fs::NamedFile::open("html/index.html")
+    match TEMPLATES.render("index.tera", &INDEX_CTX) {
+        Ok(html) => HttpResponse::Ok().content_type(ContentType::html()).body(html),
+        Err(err) => HttpResponse::InternalServerError().body(format!("{err}")),
+    }
 }
 
 #[get("/favicon.ico")]
 async fn favicon(_req: HttpRequest) -> impl Responder {
-    fs::NamedFile::open("static/favicon.ico")
+    afs::NamedFile::open("static/favicon.ico")
 }
 
 #[get("/robots.txt")]
 async fn robots(_req: HttpRequest) -> impl Responder {
-    fs::NamedFile::open("static/robots.txt")
+    afs::NamedFile::open("static/robots.txt")
 }
